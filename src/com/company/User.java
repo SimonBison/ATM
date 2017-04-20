848b06f70dfd4157485455e07bbf093e83bd8d30
SimonBison <simonpeterbison@gmail.com>
2017-04-20 13:36:00 +0200

    user details + comments

---
+++ src/com/company/User.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017-04-20.
+ */
+public class User {
+
+    private String firstName; // The first name of the user
+
+    private String lastName; // The last name of the user
+
+    private String uuid; // unique universal identifier - it is a id code(number) for the user
+
+    private byte pinHash[]; // storing user pin information
+
+    private ArrayList<Account> accounts;
+
+
+}
